ccd61a006945e472021eb200418a9e4a4ed46d80
Rahiman K <73978407+Rahiman-K@users.noreply.github.com>
2025-09-06 14:52:51 +0530

    Create 004_Operators.py

---
+++ Python/004_Operators.py
@@ -0,0 +1,73 @@
+# Arithmetic Operators
+
+a = 10
+b = 5
+
+add_result = a+b  ## addition operator
+sub_result = a-b  ## subtraction
+mult_result = a*b ## multiplication
+div_result = a/b  ## division
+floor_div = a//b  ## floor 
+moduls_result = a%b ## modolus
+
+
+exponential_result = a^b ## expotential or you can write as a ** b
+
+print("addition - ",add_result)
+print("subtraction - ",sub_result)
+print("division - ", div_result)
+print("multiplication - ",mult_result)
+print("floor_divions - ",floor_div)
+print("moduls_result - ", moduls_result)
+
+print(21/5) 
+
+print(21//5) ## this removes point values and gives just ones values
+
+
+#########################
+#  Comparision Operator #
+# 1. Equals to  ==      #
+# 2. Not Equal to !=    #
+# 3. Greater than >     #
+# 4. Less than <        #
+# 5. greather than or equal to >= #
+# 6. Less than or equal to     <= #
+###################################
+
+a = 10
+b = 10
+
+print(a==b)
+
+c = 3
+print("a !=c ", a!=c)
+print( "a > c", a > c)
+print("a < c", a < c)
+
+
+###########################
+#   LOGICAL OPERATORS     #
+# 1. AND                  #
+# 2. NOT                  #
+# 3. OR                   #  
+###########################
+
+
+X = True
+y = True
+z = False
+
+results = X and y
+print(results)
+
+
+results2 = X and z
+print(results2)
+
+results3 = X or z
+print(results3)
+
+
+x = 5
+not x
